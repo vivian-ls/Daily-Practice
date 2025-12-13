4e88f4c3ebcd0f9093109192c4bec3bfb13cfb21
CubeCat <L2106508081@outlook.com>
2025-12-13 22:30:44 +0800

    Create 线段树上二分.cpp

---
+++ model/线段树上二分.cpp
@@ -0,0 +1,44 @@
+class Seg{
+public:
+    int n;
+    vector<int> a, info;
+    Seg(int n) : n(n), info((n << 2) + 1, inf), a(n + 1) {}
+
+    void build(int i, int l, int r) {
+        if(l == r)
+            info[i] = a[l];
+        else {
+            int mid = (l + r) >> 1;
+            build(i << 1, l, mid);
+            build(i << 1 | 1, mid + 1, r);
+            info[i] = min(info[i << 1], info[i << 1 | 1]);
+        }
+    }
+    void set(int pos, int val, int l, int r, int i) {  
+        if(l == r) {
+            a[pos] = val;
+            info[i] = val;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        if(pos <= mid) set(pos, val, l, mid, i << 1);
+        else set(pos, val, mid + 1, r, i << 1 | 1);
+        info[i] = min(info[i << 1], info[i << 1 | 1]);
+    }
+    int query(int jobl, int jobr, int jobv, int l, int r, int i) {
+        if(info[i] > jobv) return -1;
+        int mid = (l + r) >> 1;
+        if(l == r) return l;
+        else if(jobl <= l && r <= jobr) {
+            if(info[i << 1] <= jobv) return query(jobl, jobr, jobv, l, mid, i << 1);
+            else return query(jobl, jobr, jobv, mid + 1, r, i << 1 | 1);
+        }else {
+            int ans = -1;
+            if(jobl <= mid) 
+                ans = query(jobl, jobr, jobv, l, mid, i << 1);
+            if(ans == -1 && jobr > mid) 
+                ans = query(jobl, jobr, jobv, mid + 1, r, i << 1 | 1);
+            return ans;
+        }
+    }
+};
