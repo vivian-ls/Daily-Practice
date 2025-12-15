3e2782770a642509b6b04b39811775e3fe6e23c7
CubeCat <L2106508081@outlook.com>
2025-12-15 16:45:57 +0800

    Update 组合数.cpp

--- model/组合数.cpp
+++ model/组合数.cpp
@@ -1,43 +1,36 @@
-const int N = 1e6 + 5;
-int fac[N], inv[N], p[N];
-void init(){
-    fac[0] = inv[0] = 1;
-    fore(i, 1, N + 1) fac[i] = fac[i - 1] * i % mod;
-    inv[N] = fpow(fac[N], mod - 2);
-    for (int i = N - 1; i >= 1; i--)
-        inv[i] = inv[i + 1] * (i + 1) % mod;
-    p[1] = 2;
-    fore(i, 2, N + 1) p[i] = p[i - 1] * 2 % mod;
+int fpow(int a, int b, int mod) {
+    if(b < 0) return 0;
+    int ans = 1;
+    while (b) { if (b & 1) ans = ans * a % mod; a = a * a % mod; b >>= 1;}
+    return ans;
 }
+
+const int N = 2e6;
+int fac[N + 5], finv[N + 5];
+int inv(int n) {
+    return fpow(n, mod - 2, mod);
+}
+
 int A(int n, int m) {
-    return fac[n] * inv[n - m] % mod;
+    if(n < 0 || m < 0 || m > n) return 0;
+    return fac[n] * finv[n - m] % mod;
 }
 
 int C(int n, int m) {
-    return fac[n] * inv[m] % mod * inv[n - m] % mod;
+    if(m < 0 || m > n) return 0;
+    return fac[n] * finv[m] % mod * finv[n - m] % mod;
 }
 
 int Catalan(int n) {  // 卡特兰数
     if(n == 0) return 1;
-    else return C(n * 2, n) * fpow(n + 1, mod - 2) % mod;
+    else return C(n * 2, n) * fpow(n + 1, mod - 2, mod) % mod;
 }
-
-int Catalan(int n, int mod) {  // when mod is not prime
-    vector<int> cnt(n * 2, 0);
-    fore(i, 2, n + 1) cnt[i] = -1;
-    fore(i, n + 2, n * 2 + 1) cnt[i] = 1;
-    for(int i = n * 2; i >= 2; i--) {
-        if(lpf[i] == 0) continue;
-        cnt[lpf[i]] += cnt[i];
-        cnt[i / lpf[i]] += cnt[i];
-        cnt[i] = 0;
-    }
-    int ans = 0;
-    fore(i, 2, n * 2 + 1) {
-        if(cnt[i] == 0) continue;
-        ans = ans * fpow(i, cnt[i], mod) % mod;
-    }
-    return ans;
+void init(){
+    fac[0] = finv[0] = 1;
+    fore(i, 1, N + 1) fac[i] = fac[i - 1] * i % mod;
+    finv[N] = inv(fac[N]);
+    for (int i = N - 1; i >= 1; i--)
+        finv[i] = finv[i + 1] * (i + 1) % mod;
 }
 
 
