2c70d2381ac9776a746d65dcd8691c13afea09dc
CubeCat <L2106508081@outlook.com>
2025-12-20 00:09:20 +0800

    Update 线段树(单点修改区间查询).cpp

--- model/线段树(单点修改区间查询).cpp
+++ model/线段树(单点修改区间查询).cpp
@@ -2,7 +2,7 @@ class Seg{
 public:
     int n;
     vector<int> a, info;
-    Seg(int n) : n(n), info((n << 2) + 1, -inf), a(n + 1) {}
+    Seg(int n) : n(n), info((n << 2) + 1, 1), a(n + 1, 1) {}
 
     void build(int i, int l, int r) {
         if(l == r)
@@ -11,7 +11,7 @@ public:
             int mid = (l + r) >> 1;
             build(i << 1, l, mid);
             build(i << 1 | 1, mid + 1, r);
-            info[i] = max(info[i << 1], info[i << 1 | 1]);
+            info[i] = info[i << 1] * info[i << 1 | 1];
         }
     }
     void set(int pos, int val, int l, int r, int i) {  
@@ -23,16 +23,20 @@ public:
         int mid = (l + r) >> 1;
         if(pos <= mid) set(pos, val, l, mid, i << 1);
         else set(pos, val, mid + 1, r, i << 1 | 1);
-        info[i] = max(info[i << 1], info[i << 1 | 1]);
+        info[i] = info[i << 1] * info[i << 1 | 1];
     }
+    void set(int pos, int val) {return set(pos, val, 1, n, 1);}
     int query(int jobl, int jobr, int l, int r, int i) {
         if(jobl <= l && jobr >= r) 
             return info[i];
-        int mid = (l + r) >> 1, left = -inf, right = -inf;
+        int mid = (l + r) >> 1, left = 1, right = 1;
         if(jobl <= mid)
             left = query(jobl, jobr, l, mid, i << 1);
         if(jobr > mid)
             right = query(jobl, jobr, mid + 1, r, i << 1 | 1);
-        return max(left, right);
+        return left * right;
     }
+    int query(int jobl, int jobr) {return query(jobl, jobr, 1, n, 1);}
+
+    int get(int pos) {return query(pos, pos);}
 };
