173f794f0d802e126201b5948020015fb1c00006
CubeCat <L2106508081@outlook.com>
2025-12-30 15:20:32 +0800

    Update ST表.cpp

--- model/ST表.cpp
+++ model/ST表.cpp
@@ -1,19 +1,19 @@
 class ST{
-public:  // 记得加上public
+public:
     int n;
     vector<vector<int>> st;
     ST(int n, vector<int>& a) : n(n){
         st = vector<vector<int>>(n + 1, vector<int>(22 + 1));
         build(n, a);
     }
-    inline int get(const int& a, const int& b) { return max(a, b); };
+    friend int get(const int a, const int b);
 
     void build(int n, vector<int>& a){
         fore(i, 1, n + 1) st[i][0] = a[i];
         for (int p = 1, t = 2; t <= n; t <<= 1, p++){
             for (int i = 1; i <= n; i++){
                 if(i + t - 1 > n) break;
-                st[i][p] = max(st[i][p - 1], st[i + (t >> 1)][p - 1]);
+                st[i][p] = get(st[i][p - 1], st[i + (t >> 1)][p - 1]);
             }
         }
     }
@@ -22,3 +22,7 @@ public:  // 记得加上public
         return get(st[l][t], st[r - (1 << t) + 1][t]);
     }
 };
+
+int get(const int a, const int b) {
+    return max(a, b);
+}
